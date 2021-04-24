0cc8eec325f521f35a44fc3e64b1a9806ea525e7
trident-kanda <tridentkandayuki@gmail.com>
2021-04-24 23:58:44 +0900

    marker

--- src/pages/marker.tsx
+++ src/pages/marker.tsx
@@ -2,15 +2,40 @@ import Head from "next/head";
 import Layout from "../components/Layout";
 import GoogleMapReact from "google-map-react";
 import Nav from "../components/Nav";
-console.log(process.env.GOOGLE_KEY);
-const latlng = { lat: 35.170915, lng: 136.881537 };
-const handleApiLoaded = ({ map, maps }: any) => {
-  new maps.Marker({
-    map,
-    position: latlng,
-  });
-};
-export default function Home() {
+import { useState } from "react";
+
+export default function Marker() {
+  const defaultLatLng = { lat: 35.170915, lng: 136.881537 };
+  const [map, setMap] = useState<any>(null);
+  const [maps, setMaps] = useState<any>(null);
+  const [marker, setMarker] = useState<any>(null);
+  const [latLng, setLatLng] = useState<any>(defaultLatLng);
+  const handleApiLoaded = (map: any, maps: any) => {
+    setMap(map);
+    setMaps(maps);
+    setMarker(
+      new maps.Marker({
+        map,
+        position: defaultLatLng,
+      })
+    );
+  };
+  const mapClick = ({ x, y, lat, lng, event }: any) => {
+    if (marker) {
+      marker.setMap(null);
+    }
+    const position = {
+      lat,
+      lng,
+    };
+    setLatLng(position);
+    setMarker(
+      new maps.Marker({
+        map,
+        position: position,
+      })
+    );
+  };
   return (
     <Layout>
       <Nav />
@@ -18,11 +43,19 @@ export default function Home() {
       <div className="bg-white p-6 h-96 sm:rounded-lg shadow ">
         <GoogleMapReact
           defaultZoom={15}
-          defaultCenter={latlng}
+          defaultCenter={defaultLatLng}
           yesIWantToUseGoogleMapApiInternals={true}
           bootstrapURLKeys={{ key: process.env.GOOGLE_KEY as string }}
-          onGoogleApiLoaded={handleApiLoaded}
+          onGoogleApiLoaded={({ map, maps }) => {
+            handleApiLoaded(map, maps);
+          }}
+          onClick={mapClick}
         ></GoogleMapReact>
+      </div>
+      <div className="mt-4" />
+      <div className="p-6 bg-white shadow sm:rounded-lg">
+        <p className=" text-center">Lat:{latLng.lat}</p>
+        <p className=" text-center">Lng:{latLng.lng}</p>
       </div>
     </Layout>
   );

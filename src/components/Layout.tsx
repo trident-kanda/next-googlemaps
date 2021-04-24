aaef924a2b7a2bc1344c9b49be50f73f94bb9b1b
trident-kanda <tridentkandayuki@gmail.com>
2021-04-24 15:21:22 +0900

    design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode, VFC } from "react";
 const Layout: VFC<{ children: ReactNode }> = (props) => {
   return (
     <div className=" min-h-screen bg-gray-100">
-      <div className="w-full max-w-3xl lg:max-w-5xl mx-auto pt-6 sm:pt-8">
+      <div className="w-full max-w-3xl lg:max-w-5xl mx-auto pt-6 sm:pt-8 sm:px-5">
         <main>{props.children}</main>
       </div>
     </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,20 +4,26 @@ const Nav = () => {
   const { pathname } = useRouter();
   return (
     <nav className="sm:rounded-lg bg-white flex shadow">
-      {}
       <Link href="/">
-        <a className="flex-1 py-4 text-center font-medium text-xl text-gray-500 hover:bg-gray-50 hover:text-black">
-          基本
+        <a className="flex-1 py-4 text-center font-medium text-xl text-gray-500 hover:bg-gray-50 hover:text-black rounded-l-lg">
+          {pathname === "/" && <span className=" text-green-500">基本</span>}
+          {pathname !== "/" && <span>基本</span>}
         </a>
       </Link>
       <Link href="/marker">
-        <a className="flex-1 py-4 text-center font-medium text-xl text-gray-500 hover:bg-gray-50 hover:text-black">
-          マーカーセット
+        <a className="flex-1 py-4 text-center font-medium text-xl text-gray-500 hover:bg-gray-50 hover:text-black border-l border-r border-gray-100">
+          {pathname === "/marker" && (
+            <span className=" text-green-500">クリック</span>
+          )}
+          {pathname !== "/marker" && <span>クリック</span>}
         </a>
       </Link>
       <Link href="/geocoder">
-        <a className="flex-1 py-4 text-center font-medium text-xl text-gray-500 hover:bg-gray-50 hover:text-black">
-          検索
+        <a className="flex-1 py-4 text-center font-medium text-xl text-gray-500 hover:bg-gray-50 hover:text-black rounded-r-lg">
+          {pathname === "/geocoder" && (
+            <span className=" text-green-500">現在地</span>
+          )}
+          {pathname !== "/geocoder" && <span>現在地</span>}
         </a>
       </Link>
     </nav>

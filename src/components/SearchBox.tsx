12afe07d96ea1366b0e47ff37e9ff75b73b84e50
trident-kanda <tridentkandayuki@gmail.com>
2021-04-26 18:23:35 +0900

    geolocation

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,27 +1,36 @@
 import { MapOptions, Maps } from "google-map-react";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
-const SearchBox = ({ maps, map, inputMarker }: any) => {
+const SearchBox = ({ maps, map, inputMarker, marker }: any) => {
+  const input = useRef(null);
+  const searchBox: any = useRef(null);
+  const placesChanged = () => {};
   useEffect(() => {
-    const input: any = document.getElementById("input");
-    const searchBox = new maps.places.SearchBox(input);
-    searchBox.addListener("places_changed", () => {
-      const places = searchBox.getPlaces();
+    searchBox.current = new maps.places.SearchBox(input.current);
+    map.controls[maps.ControlPosition.TOP_LEFT].push(input.current);
+    searchBox.current.addListener("places_changed", () => {
+      const places = searchBox.current.getPlaces();
       if (places.length === 0) {
         return;
       }
-      console.log(places[0]);
-      // inputMarker(
-      //   new maps.Marker({
-      //     map,
-      //     position: defaultLatLng,
-      //   })
-      // )
+      const bounds = new maps.LatLngBounds();
+      const location = places[0].geometry.location;
+      console.log(location);
+      console.log(bounds);
+      marker.setMap(null);
+      inputMarker(
+        new maps.Marker({
+          map,
+          position: location,
+        })
+      );
+      map.panTo(location);
     });
-  }, []);
+  }, [maps]);
   return (
     <input
-      id="input"
+      type="text"
+      ref={input}
       className=" border-2 border-gray-500 rounded focus:outline-none focus:border-green-500 p-1 w-1/2 absolute right-full"
     />
   );
--- src/pages/geocoder.tsx
+++ src/pages/geocoder.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
-import GoogleMapReact, { MapOptions } from "google-map-react";
+import GoogleMapReact from "google-map-react";
 import Nav from "../components/Nav";
 import { useCallback, useEffect, useState } from "react";
 import SearchBox from "../components/SearchBox";
@@ -11,7 +11,6 @@ export default function Home() {
   const [marker, setMarker] = useState<any>(null);
   const [apiReady, changeApi] = useState(false);
   const defaultLatLng = { lat: 35.170915, lng: 136.881537 };
-
   const inputMarker = useCallback(
     (mark: any) => {
       setMarker(mark);
@@ -19,17 +18,15 @@ export default function Home() {
     [setMarker]
   );
   const handleApiLoaded = ({ map, maps }: any) => {
-    setMap(map);
-    setMaps(maps);
     setMarker(
       new maps.Marker({
         map,
         position: defaultLatLng,
       })
     );
+    setMap(map);
+    setMaps(maps);
     changeApi(true);
-    const input = document.getElementById("input");
-    map.controls[maps.ControlPosition.TOP_LEFT].push(input);
   };
 
   return (
@@ -37,10 +34,15 @@ export default function Home() {
       <Nav />
       <div className="mt-4" />
       <div className="bg-white p-6  sm:rounded-lg shadow ">
-        {apiReady !== false && (
-          <SearchBox maps={maps} map={map} inputMarker={inputMarker} />
-        )}
         <div className="h-96 ">
+          {apiReady && (
+            <SearchBox
+              maps={maps}
+              map={map}
+              inputMarker={inputMarker}
+              marker={marker}
+            />
+          )}
           <GoogleMapReact
             defaultZoom={15}
             defaultCenter={defaultLatLng}
